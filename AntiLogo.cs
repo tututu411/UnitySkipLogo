eee782b36b66c5f7e00b0550993cf41954d3546c
何tutu <65388278+tututu411@users.noreply.github.com>
2023-10-08 10:16:33 +0800

    Add files via upload
    unity跳过启动Logo

---
+++ AntiLogo.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class AntiLogo
+{
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSplashScreen)]
+    private static void Function()
+    {
+        Task.Run(() => { SplashScreen.Stop(SplashScreen.StopBehavior.StopImmediate); });
+    }
+}
